a6c691df2712d69a2dcbea1a221ee80f0dc587c3
Curtis A Lee <leecurtis353@yahoo.co.uk>
2021-03-10 09:59:54 +0000

    Initial Commit

---
+++ Designcode/DesigncodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesigncodeApp.swift
+//  Designcode
+//
+//  Created by Curtis Lee on 10/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct DesigncodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
