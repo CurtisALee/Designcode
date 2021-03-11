5cdf8e5e027e99777478a488ef3b9c87ab55a174
Curtis A Lee <leecurtis353@yahoo.co.uk>
2021-03-11 13:47:52 +0000

    Passing data from array into a loop

--- Designcode/HomeView.swift
+++ Designcode/HomeView.swift
@@ -18,17 +18,19 @@ struct HomeView: View {
                 
                 Spacer()
                 
-                // The shared value is then called here having been declared at the component level
+                // The shared value is then called here having been declared at the component level (see Home.swift file)
                 AvatarView(showProfile: $showProfile)
             }
             .padding(.horizontal)
             .padding(.leading, 14)
             .padding(.top, 30)
             
+            // Rather than hardcode the number of items in the loop, we can instead call the sectionData variable from the array, and let the loop figure out how many iterations we're passing it (which in this case in 3)
+            
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 30) {
-                    ForEach(0 ..< 5) { item in
-                        SectionView()
+                    ForEach(sectionData) { item in
+                        SectionView(section: item)
                     }
                 }
                 .padding(30)
@@ -42,28 +44,32 @@ struct HomeView: View {
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView(showProfile: .constant(false)) // This prevents the need to declare a state if there is no state present in the current view
+        HomeView(showProfile: .constant(false)) // This prevents the need to declare a state if there is no state present in the current view (see Home.swift file)
     }
 }
 
 struct SectionView: View {
+    
+    // Create a new variable with the value being the data structure defined at the bottom
+    var section: Section
+    
     var body: some View {
         VStack {
             HStack(alignment: .top) {
-                Text("Prototype designs in SwiftUI")
+                Text(section.title) // Link variable to the prop based on the root var of this view, e.g. title
                     .font(.system(size: 24, weight: .bold))
                     .frame(width: 160, alignment: .leading)
                     .foregroundColor(.white)
                 
                 Spacer()
                 
-                Image("Logo1")
+                Image(section.logo) // logo prop example etc
             }
             
-            Text("18 Sections".uppercased())
+            Text(section.text.uppercased())
                 .frame(maxWidth: .infinity, alignment: .leading)
             
-            Image("Card1")
+            section.image
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 210)
@@ -71,8 +77,27 @@ struct SectionView: View {
         .padding(.top, 20)
         .padding(.horizontal, 20)
         .frame(width: 275, height: 275)
-        .background(Color("card1"))
+        .background(section.color)
         .cornerRadius(30)
-        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
+        .shadow(color: section.color.opacity(0.3), radius: 20, x: 0, y: 20)
     }
 }
+
+// Structure for the card containing all of the props we'll pass to it
+struct Section: Identifiable {
+    var id = UUID()
+    var title: String
+    var text: String
+    var logo: String
+    var image: Image
+    var color: Color
+}
+
+// This also uses imageLiteral which is cool: Image(uiImage: imageType)
+// Array of all of the data we need to pass to the card component, and reference in the loop
+
+let sectionData = [
+    Section(title: "Prototype designs in SwiftUI", text: "18 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card4")), color: Color("card1")),
+    Section(title: "Buid a SwiftUI app", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Background1")), color: Color("card2")),
+    Section(title: "SwiftUI Advanced", text: "20 Sections", logo: "Logo1", image: Image(uiImage: #imageLiteral(resourceName: "Card2")), color: Color("card3"))
+]
