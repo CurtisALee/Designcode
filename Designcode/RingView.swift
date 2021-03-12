21826604d17199d90cbc987bc0b9aba42055be94
Curtis A Lee <leecurtis353@yahoo.co.uk>
2021-03-12 15:47:42 +0000

    Creating RingView animation

--- Designcode/RingView.swift
+++ Designcode/RingView.swift
@@ -10,14 +10,16 @@ import SwiftUI
 struct RingView: View {
     var color1 = #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1)
     var color2 = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
-    var width: CGFloat = 88 // Expects width (and height) to be a CGFloat, so we must specific here in order to use use in .frame()
-    var height: CGFloat = 88
+    var width: CGFloat = 300 // Expects width (and height) to be a CGFloat, so we must specify here in order to use in .frame()
+    var height: CGFloat = 300
     var percent: CGFloat = 88 // This can now be dyamnically changed
+    
+    @Binding var show: Bool // Control animation state outside of this file
     
     var body: some View {
         // Use a let as the value won't be changing directly
-        let multiplier = width / 44
-        let progress = 1 - percent / 100 // 1 is the whole number, minus the percent out of 100
+        let multiplier = width / 44 // = 2
+        let progress = 1 - percent / 100 // 1 means no progress, minus the percent out of 100
         
         // Must include a 'return' if you use a variable inside the body
         return ZStack {
@@ -26,7 +28,7 @@ struct RingView: View {
                 .frame(width: width, height: height)
             
             Circle()
-                .trim(from: progress, to: 1)
+                .trim(from: show ? progress : 1, to: 1) // True: show progress, else: show none (1)
                 .stroke(
                     LinearGradient(gradient: Gradient(colors: [Color(color1), Color(color2)]), startPoint: .topTrailing, endPoint: .bottomLeading),
                     style: StrokeStyle(
@@ -39,14 +41,17 @@ struct RingView: View {
                 .shadow(color: Color(color2).opacity(0.1), radius: 3 * multiplier, x: 0, y: 3 * multiplier)
             
             Text("\(Int(percent))%") // Calling the percent variable here
-                .font(.system(size: 14 * multiplier))
+                .font(.system(size: 14 * multiplier)) // e.g. 14 x 2 = 28
                 .fontWeight(.bold)
+                .onTapGesture {
+                    self.show.toggle()
+            }
         }
     }
 }
 
 struct RingView_Previews: PreviewProvider {
     static var previews: some View {
-        RingView()
+        RingView(show: .constant(true))
     }
 }
