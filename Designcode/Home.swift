01b54dc25727db85adaad69c19a968f11d1a5385
Curtis A Lee <leecurtis353@yahoo.co.uk>
2021-03-16 14:35:04 +0000

    Dark Mode

--- Designcode/Home.swift
+++ Designcode/Home.swift
@@ -14,18 +14,18 @@ struct Home: View {
     
     var body: some View {
         ZStack {
-            Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+            Color("background2")
                 .edgesIgnoringSafeArea(.all) // Prevents default space on top and bottom
             
             HomeView(showProfile: $showProfile, showContent: $showContent) // The shared state value is then called here having been declared at the component level
                 .padding(.top, 44)
                 .background(
                     VStack {
-                        LinearGradient(gradient: Gradient(colors: [Color("background2"), Color.white]), startPoint: .top, endPoint: .bottom)
+                        LinearGradient(gradient: Gradient(colors: [Color("background2"), Color("background1")]), startPoint: .top, endPoint: .bottom)
                             .frame(height: 200)
                         Spacer()
                     }
-                    .background(Color.white)
+                    .background(Color("background1"))
                 )
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
                 .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
@@ -59,7 +59,7 @@ struct Home: View {
             // In this file, it is set to a false state by default, but using the bind method we can change that to true onTap.
             // This gesture is defined in HomeView.swift, and then called here
             if showContent {
-                Color.white.edgesIgnoringSafeArea(.all)
+                BlurView(style: .systemMaterial).edgesIgnoringSafeArea(.all)
                 
                 ContentView()
                 
@@ -91,7 +91,7 @@ struct Home: View {
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Home()
+        Home().environment(\.colorScheme, .dark)
     }
 }
 
